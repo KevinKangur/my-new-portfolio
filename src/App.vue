181d6ed603c17b1312bfadf5a80bc71a619c58d5
KevinKangur <kevinkangur13@gmail.com>
2023-11-02 11:42:19 +0200

    first commit, wip

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <header class="flex justify-between bg-[#DAFFFB] py-10 px-20">
+    <h1 class="text-[#04364A] text-4xl font-['Poppins'] font-semibold">Kevin Kangur</h1>
+    <div class="flex gap-20">
+      <a class="text-[#04364A] text-2xl font-['Poppins'] font-medium hover:text-[#176B87]" href="#">Home</a>
+      <a class="text-[#04364A] text-2xl font-['Poppins'] font-medium hover:text-[#176B87]" href="#">About</a>
+      <a class="text-[#04364A] text-2xl font-['Poppins'] font-medium hover:text-[#176B87]" href="#">Work</a>
+    </div>
+    <div class="flex gap-10">
+      <a href="https://twitter.com/kangur_kevin"><img class="w-10" src="src/img/twitter-icon.png" alt=""></a>
+      <a href="https://github.com/KevinKangur"><img class="w-10" src="src/img/github-icon.png" alt=""></a>
+      <a href="#"><img class="w-10" src="src/img/facebook-icon.png" alt=""></a>
+    </div>
+  </header>
+</template>
